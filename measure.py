838fbee852694a21de868a154bf414d092532f95
tanmoy7989 <tanmoy.7989@gmail.com>
2016-03-09 15:49:34 -0800

    added cluster size calculation

--- measure.py
+++ measure.py
@@ -10,14 +10,29 @@ import parse_potential
 import cgmodel as cg
 sys.path.append('~/'); from selLDCut import *
 
-def getBoxL(LammpsTraj):
-    trj = pickleTraj(LammpsTraj)
+# flags and other globals
+doBlockAvg = True ; NBlocks = 5
+MeasureFreq = 1
+Normalize = True
+useAtomTypes = True
+Prefix = 'Measure'
+
+
+
+def __AtomName2Type(AtomNames):
+	AtomTypes = []
+	[AtomTypes.append(int(x)) for x in AtomNames]
+	return np.array(AtomTypes)
+
+
+def getBoxL(Traj):
+    trj = pickleTraj(Traj)
     return trj.FrameData['BoxL']
+
 
 def gen_Srel_rc(LammpsTraj, LDCuts = []):
     # prepare the system
     cg.LammpsTraj = LammpsTraj
-    Prefix = cg.makeSysPrefix()
     cg.LDCutBW = LDCuts[0]
     cg.BoxL = getBoxL(LammpsTraj)
     Sys = cg.makeSys()
@@ -45,11 +60,58 @@ def gen_fsw(LammpsTraj, LDCuts = [], NB = 250, NW = 250):
     fsw.LDCuts = LDCuts ; fsw.LDDelta = 1.2
     fsw.Prefix = 'NB%dNW%d' % (NB, NW) ; fsw.genFileNames()
 
-    # calculate rdf
-    #fsw.makeRDF()
+    #calculate rdf
+    fsw.makeRDF()
     
     # calculate maximal correlation
     fsw.makeFSWCorrelation(rdfCut = 4.) 
     #based on NB250 B-W rdf (but unusual)
     fsw.calcCorrelation()
     
+
+def makeCluster(Traj, Cut = None, Atom = 'B', NAtom = 250):
+	Trj = pickleTraj(Traj)
+	FrameRange = range(0, len(Trj), MeasureFreq)
+	NFrames = len(FrameRange)
+	BoxL = getBoxL(Traj)
+
+	AtomNames = Trj.AtomNames
+	if useAtomTypes: Trj.AtomTypes = __AtomName2Type(Trj.AtomNames)
+	if Atom == 'B': ClustAtomType = 1
+	else: ClustAtomType = 2
+	Inds = np.where(Trj.AtomTypes == ClustAtomType)
+
+	bin_centers = range(0, NAtom+1)
+	bin_val_measure = np.zeros([NFrames, NAtom+1], np.float64)
+	bin_val_hist = np.zeros(NAtom+1)
+	err = None
+
+	pb = sim.utility.ProgressBar(Text = 'Calculating cluster dist', Steps = NFrames)
+	count = 0
+	for frame in FrameRange:
+		Pos = Trj[frame][Inds]
+		clustdist, clustgroups = sim.geom.ClusterStats(Pos = Pos, BoxL = BoxL, Cutoff = Cut)
+		bin_val_measure[count, :] = np.array(clustdist)
+		pb.Update(count)
+		count += 1
+
+	bin_val_hist = np.mean(bin_val_measure, axis = 0)
+	if Normalize: bin_val_hist /= np.sum(bin_val_hist)
+
+	if doBlockAvg:
+		print '\n\n'
+		err = np.zeros(NAtom + 1)
+		bin_val_block = np.zeros([NBlocks, NAtom+1], np.float64)
+		BlockLen = int(NFrames/NBlocks)
+		count = 0
+		pb = sim.utility.ProgressBar(Text = 'Calculating error', Steps = NBlocks)
+		for block in range(NBlocks):
+			bin_val_block[block] = np.mean(bin_val_measure[count:count+BlockLen], axis = 0)
+			bin_val_block[block] /= np.sum(bin_val_block[block])
+			pb.Update(block)
+			count += BlockLen
+
+		err = np.std(bin_val_block, axis = 0)	
+
+	MeasurePickleName = Prefix + '.pickle'
+	pickle.dump((bin_val_measure, (bin_centers, bin_val_hist, err)), open(MeasurePickleName, 'w'))
